95495d0354a3826955f3c1b41dec36fbc62d2614
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-03-30 15:36:04 +0200

    add popup and update mobile version

--- app.js
+++ app.js
@@ -52,4 +52,62 @@ function afficherBtns() {
 /***************************************POPUP********************************************** */
 
 const btnPopup = document.querySelectorAll('#container button');
-console.log(btnPopup);// affiche un tableau de 8 boutons dans la console
+console.log(btnPopup);// affiche un tableau de boutons dans la console
+
+//initialisation et affectation des variables
+const popup = document.createElement("div");
+const popupContent = document.createElement("div");
+const row = document.createElement("div");
+const colText = document.createElement("div");
+const img = document.createElement("img");
+const titre = document.createElement("h3");
+const par = document.createElement("p");
+const btnClose = document.createElement("span");
+
+//Creation de la popup
+popup.setAttribute('id', 'popup');
+popupContent.setAttribute('id', 'popup-content');
+row.setAttribute('id', 'row-popup');
+colText.setAttribute('id', 'col-text');
+img.src = 'img/img1.jpg';
+img.setAttribute('id', 'img-popup');
+titre.innerText = 'Lorem ipsum dolor';
+par.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolore eveniet non pariatur consectetur exercitationem labore molestias sint nisi quos.';
+btnClose.innerHTML = "&times;";
+btnClose.setAttribute('id', 'close');
+
+//Construction et ajout de la popup dans le html
+popupContent.appendChild(btnClose);
+colText.appendChild(titre);
+colText.appendChild(par);
+row.appendChild(img);
+row.appendChild(colText);
+popupContent.appendChild(row);
+popup.appendChild(popupContent);
+document.body.appendChild(popup);
+
+// Quand l'utilisateur click sur <span> (x), ferme la popup
+btnClose.onclick = function () {
+    popup.style.display = "none";
+}
+
+// Quand l'utilisateur click ferme en dehor de la popup, ferme la popup
+window.onclick = function (event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+
+// rajoute un evenement click sur chaque bouton pour afficher la popup
+for (let index = 0; index < btnPopup.length; index++) {
+    btnPopup[index].addEventListener('click', () => {
+        console.log(index+1);
+        displayPopup(index);
+    })
+}
+
+// fonction qui affiche une popup
+function displayPopup(index) {
+    img.src = 'img/img'+(index+1)+'.jpg';
+    popup.style.display = "block";
+}
